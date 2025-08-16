87c7e5bfe3c014beb5d0458a4ebb88bdccaaafda
Weyn <gordijcukvlad64@gmail.com>
2025-08-16 22:33:14 +0300

    Add .bat script to generate Visual Studio solution

--- compile_win.bat
+++ compile_win.bat
@@ -1,3 +1,22 @@
-mkdir build
-cd build
-cmake ..
+@echo off
+setlocal
+
+set BUILD_DIR=build
+
+set GENERATOR="Visual Studio 17 2022"
+
+if exist %BUILD_DIR% (
+    echo Removing old build directory...
+    rmdir /s /q %BUILD_DIR%
+)
+
+mkdir %BUILD_DIR%
+
+cmake -B %BUILD_DIR% -S . -G %GENERATOR%
+
+if exist %BUILD_DIR%\*.sln (
+    for %%f in (%BUILD_DIR%\*.sln) do start "" "%%f"
+)
+
+endlocal
+pause
